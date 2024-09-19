86bbf934c1362780289ec4561d796e1a1b7d1435
hoshikuzuryuu <yaminoiroha@gmail.com>
2024-09-19 09:59:54 -0700

    im done!

--- Clock.java
+++ Clock.java
@@ -2,9 +2,41 @@ public class Clock
 {
   public static void main(String[] args)
   {
-    // test code here
+    int time = 16; 
+    System.out.println("According to your clock that reads " + time + ", the time of day is: ");
+    timeOfDay(time);
   }
 
-  // write code for timeOfDay here
+  public static void timeOfDay(int time)
+  {
+    if (time == 0 || time == 24) 
+    {
+      System.out.println("midnight");
+    }
+    if (time == 12) 
+    {
+      System.out.println("noon");
+    }
+    if (time == 18) 
+    {
+      System.out.println("dusk");
+    }
+    if (time > 0 & time < 12) 
+    {
+      System.out.println("morning");
+    }
+    if (time > 12 & time < 18) 
+    {
+      System.out.println("afternoon");
+    }
+    if (time > 18 & time < 24) 
+    {
+      System.out.println("midnight");
+    }
+    if (time < 0 || time > 24) 
+    {
+      System.out.println("Error; please input a number between 0 and 24");
+    }
+  }
 
 }
--- Coins.java
+++ Coins.java
@@ -2,9 +2,22 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    int cents = 1234; 
+    System.out.println("Based on the " + cents + " cents you have, your converted money is: ");
+    coinConverter(cents);
   }
 
-  // write code for coinConverter
-
+  public static void coinConverter(int cents) 
+  {
+    int dollars = cents/100;
+    int quarters = (cents - (dollars*100))/25;
+    int dimes = (cents - ((dollars*100) + (quarters*25)))/10;
+    int nickels = (cents - ((dollars*100) + (quarters*25) + (dimes*10)))/5;
+    int pennies = (cents - ((dollars*100) + (quarters*25) + (dimes*10) + (nickels*5)));
+    System.out.println("Dollar Bills: " + (dollars));
+    System.out.println("Quarters: " + quarters);
+    System.out.println("Dimes: " + dimes);
+    System.out.println("Nickels: " + nickels);
+    System.out.println("Pennies: " + pennies);
+  }
 }
